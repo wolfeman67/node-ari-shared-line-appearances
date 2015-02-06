363f8acf8ccd4255050a6796969378ace1974506
Tyler Cambron <tyler@tcambron.digium.internal>
2015-02-06 15:45:39 -0600

    Originated channel is now added to a bridge.
    The program checks for an available bridge. If a bridge is available,
    then that bridge is reused. If no bridges are active, a new bridge
    is created. Once the bridge is created, a channel is originated to
    a designated endpoint, and upon answering the call, the channel is
    added to the bridge.

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ var sla = require('./lib/sla.js');
 ari.connect('http://127.0.0.1:8088', 'user', 'pass', clientLoaded);
 
 function clientLoaded (err, client){
-    console.log(sla());}
+  client.start('hello');
+  var outgoing = client.Channel();
+  sla(client, outgoing, function(){
+    console.log('done');
+  });
+}
