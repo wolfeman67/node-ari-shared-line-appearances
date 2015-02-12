f565ca339458196a0d208163be88dd4ab06450a5
Tyler Cambron <tyler@tcambron.digium.internal>
2015-02-12 13:49:44 -0600

    Implemented promises
    Replaced all callbacks to use promises from the Q promise library.
    Much of the file has been restructured because of this. Module.exports
    now contains nested then() statements instead of each function
    calling the next.

--- app.js
+++ app.js
@@ -4,26 +4,36 @@
 var ari = require('ari-client');
 var util = require('util');
 var sla = require('./lib/sla.js');
+var Q = require('q');
 
-ari.connect('http://127.0.0.1:8088', 'user', 'pass', clientLoaded);
+var connect = Q.denodeify(ari.connect);
+
+connect('http://127.0.0.1:8088', 'user', 'pass')
+  .then(function (client) {
+    clientLoaded(client);
+  })
+  .catch(errHandler);
 
 /**
  * Starts Stasis app 'sla' and initiates SLA application after ARI connection.
- * @param {Object} err - Error message.
  * @param {Object} client - Object that contains information from the ARI 
  *   connection.
  */
-function clientLoaded (err, client) {
-  if (err) {
-    throw err;
-  }
+function clientLoaded (client) {
   client.start('sla');
-  sla(client, function(err) {
-    if (err !== null) {
-      throw err;
-    } else {
-      console.log('Application Completed');
+  sla(client)
+    .then(console.log)
+    .catch(errHandler)
+    .finally(function () {
       process.exit(0);
-    }
-  });
+    });
+}
+
+/**
+ * Handles errors found in application.
+ * @param {Object} err - error from application.
+ */
+function errHandler(err) {
+  console.error(err);
+  throw err;
 }
