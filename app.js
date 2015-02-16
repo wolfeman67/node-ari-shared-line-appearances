e7a039167173b6e5bd8fdacb5ded63042863cf77
Tyler Cambron <tyler@tcambron.digium.internal>
2015-02-16 13:46:15 -0600

    Originated channel is now added to a bridge.
    The program checks for an available bridge. If a bridge is available,
    then that bridge is reused. If no bridges are active, a new bridge
    is created. Once the bridge is created, a channel is originated to
    a designated endpoint, and upon answering the call, the channel is
    added to the bridge.

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+/*jslint node: true */  
 'use strict';
 
 var ari = require('ari-client');
 var util = require('util');
 var sla = require('./lib/sla.js');
+var Q = require('q');
 
-ari.connect('http://127.0.0.1:8088', 'user', 'pass', clientLoaded);
+var connect = Q.denodeify(ari.connect);
 
-function clientLoaded (err, client){
-    console.log(sla());}
+connect('http://127.0.0.1:8088', 'user', 'pass')
+  .then(clientLoaded)
+  .catch(errHandler)
+  .done();
+
+/**
+ * Starts Stasis app 'sla' and initiates SLA application after ARI connection.
+ * @param {Object} client - Object that contains information from the ARI 
+ *   connection.
+ */
+function clientLoaded (client) {
+  client.start('sla');
+  sla(client)
+    .then(console.log)
+    .catch(errHandler)
+    .done();
+}
+
+/**
+ * Handles errors found in application.
+ * @param {Object} err - error from application.
+ */
+function errHandler(err) {
+  console.error(err);
+  throw err;
+}
