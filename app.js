7833888740255bf3217f46432047cbd704de55eb
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2015-04-28 13:08:27 -0500

    app.js now simply calls bootstrap.

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 'use strict';
 
-var ari = require('ari-client');
-var sla = require('./lib/sla.js');
-var errHandler = require('./lib/errHandler.js');
-var Q = require('q');
+var bootstrap = require('./lib/sla.bootstrap.js');
 
-var connect = Q.denodeify(ari.connect);
-var confFile;
-if (confFile = process.argv[2]) {
-  connect('http://127.0.0.1:8088', 'user', 'pass')
-    .then(clientLoaded)
-    .catch(errHandler)
-    .done();
-} else {
+var confFilePath = process.argv[2];
+
+if (!confFilePath) {
   throw new Error('No configuration file specified');
 }
 
-/**
- * Waits for a StasisStart event before going into the main SLA module
- * @param {Object} client - Object that contains information from the ARI
- *   connection.
- */
-function clientLoaded (client) {
-  client.start('sla');
-  client.on('StasisStart', function(event, channel) {
-    if (event.args[0] !== 'dialed') {
-      var extension = event.args[0];
-      sla(client, confFile, channel, extension)
-        .catch(errHandler)
-        .done();
-    }
-  });
-}
+bootstrap(confFilePath);
