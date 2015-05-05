3ea8944da959444acc7c7a37b3d05994a4a2e87e
Chris Wolfe <chris@cwolfe.digium.internal>
2015-05-05 14:35:14 -0500

    Major application refactoring.
    Modularized functions, made a state machine which contains application
    logic and event handlers, etc.

--- app.js
+++ app.js
@@ -1,43 +1,11 @@
 'use strict';
 
-var ari = require('ari-client');
-var sla = require('./lib/sla.js');
-var errHandler = require('./lib/errHandler.js');
-var Q = require('q');
+var bootstrap = require('./lib/sla.bootstrap.js');
 
-var connect = Q.denodeify(ari.connect);
-var confFile;
-if (confFile = process.argv[2]) {
-  connect('http://127.0.0.1:8088', 'user', 'pass')
-    .then(clientLoaded)
-    .catch(errHandler)
-    .done();
-} else {
+var confFilePath = process.argv[2];
+
+if (!confFilePath) {
   throw new Error('No configuration file specified');
 }
 
-/**
- * Checks if the argument tied to the StasisStart event is dialed (not inbound)
- * @param {String} argument - The argument (either an extension # or dialed)
- */
-function isDialed(argument) {
-  return argument === 'dialed';
-}
-
-/**
- * Waits for a StasisStart event before going into the main SLA module
- * @param {Object} client - Object that contains information from the ARI
- *   connection.
- */
-function clientLoaded (client) {
-  client.start('sla');
-  client.on('StasisStart', function(event, channel) {
-    if (!isDialed(event.args[0])) {
-      var extension = event.args[0];
-      sla(client, confFile, channel, extension)
-        .then(console.log)
-        .catch(errHandler)
-        .done();
-    }
-  });
-}
+bootstrap(confFilePath);
